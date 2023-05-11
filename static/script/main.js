642ca30a5d6ee1b2c3570c5d03b544cf0cd00ad7
Chirag Singh <chirag.singh3489@gmail.com>
2023-05-11 13:43:38 -0700

    bug ifx

--- static/script/main.js
+++ static/script/main.js
@@ -15,6 +15,31 @@ function finalizeUpload() {
     req.send();
 }
 
+function sendFile() {
+    if (globalThis.requestQueue.length == 0) return;
+    let r = new XMLHttpRequest();
+    let formData = new FormData();
+    r.onreadystatechange = function () {
+        if (r.readyState == 4 && r.status == 200) {
+            globalThis.recvCount++;
+            document.getElementById('pbarv').value = globalThis.recvCount;
+
+            if (globalThis.recvCount == document.getElementById('ctrl').files.length) {
+                finalizeUpload();
+            }
+            else {
+                sendFile();
+            }
+        }
+    }
+
+    formData.append("file", globalThis.requestQueue[0]);
+    r.open("POST", 'http://localhost:5000/src');
+    r.send(formData);
+
+    globalThis.requestQueue.shift();
+}
+
 function onFolderSelect() {
     let req = new XMLHttpRequest();
     req.onreadystatechange = function () {
@@ -22,26 +47,12 @@ function onFolderSelect() {
             globalThis.recvCount = 0;
             document.getElementById('status').innerHTML = 'Uploading...';
             document.getElementById('pbar').innerHTML =
-                `<meter id='pbarv' min="0" max="${document.getElementById('ctrl').files.length}" value="0"></meter>`
-            Array.from(document.getElementById('ctrl').files).forEach(e => {
-                let r = new XMLHttpRequest();
-                let formData = new FormData();
+                `<meter id='pbarv' min="0" max="${document.getElementById('ctrl').files.length}" value="0"></meter>`;
 
-                r.onreadystatechange = function () {
-                    if (r.readyState == 4 && r.status == 200) {
-                        globalThis.recvCount++;
-                        document.getElementById('pbarv').value = globalThis.recvCount;
-
-                        if (globalThis.recvCount == document.getElementById('ctrl').files.length) {
-                            finalizeUpload();
-                        }
-                    }
-                }
-
-                formData.append("file", e);
-                r.open("POST", 'http://localhost:5000/src');
-                r.send(formData);
-            });
+            globalThis.requestQueue = Array.from(document.getElementById('ctrl').files);
+            for (let i = 0; i < 20; i++) {
+                sendFile();
+            }
         }
     }
     req.open('POST', 'http://localhost:5000/reset');
